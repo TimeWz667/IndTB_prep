f88a3d93515e937edbb9b01c355bb3f3ee6bc9b7
TimeWz667 <TimeWz667@gmail.com>
2023-07-07 15:51:50 +0100

    Edit README.md

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # IndiaTBprep
 
-Public data preprocess for india TB modelling
+Data preprocess for India TB modelling
+
+
+## Data sources
+
+- World Urbanization Prospects
+- World Population Prospects
+- WHO
+  - Burden estimates
+  - Case notification
+- TBPS, India, 2019-2021
+- Drug Sales
+- India TB report
+  - Case notification
+  - Treatment outcome
+
+
+## Maintainer
+
+**Chu-Chang Ku**
+
+- cku@ic.ac.uk
+- TimeWz667@gmail.com
+- DIDE, Imperial College London
+
+
--- main.R
+++ main.R
@@ -2,21 +2,19 @@ rm(list = ls())
 
 
 scripts <- c(
-  "0_fetch_data.R", 
-  "1_data_arti.R", 
-  "1_data_burden.R", 
-  "1_data_demography.R", 
-  "1_data_ind_report.R", 
-  "1_data_notification.R",
-  "1_data_prev.R",
-  "1_data_prev_adj.R",
-  "1_data_tx.R",
-  "2_pars_demography.R", 
-  "2_pars_cascade.R"
+  "Parse_ARTI.R", 
+  "Parse_DrugSale.R",      
+  "Parse_ITR.R", 
+  "Parse_Pop.R", 
+  "Parse_TBPS.R", 
+  "Parse_WHO.R",
+  "Collect_Pars.R", 
+  "Collect_Targets_Region.R", 
+  "Collect_Targets_State.R"
 )
 
 
 for (script in scripts) {
-  source(here::here("scripts", script))
+  source(here::here("R", script))
 }
 
